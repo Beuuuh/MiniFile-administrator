6427f6506edae7e6e8b8b743977a08ea3841d0ec
Beuuuh <belldiscord8768@hotmail.com>
2026-01-08 00:52:10 -0300

    feat: procura por arquivos de qualquer tipo na pasta pre determinada

--- main.c
+++ main.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int HasFiles() {
+    //TODO: dinamizar para qualquer pasta
+    system("dir /b C:\\Users\\USER\\Desktop\\MiniFile-administrator\\MiniFile-administrator\\output\\teste > temp.txt");
+    FILE *f;
+    int found = 0;
+    char line[256];
+    
+    f = fopen("temp.txt", "r");
+
+    if (f == NULL) {
+        remove("temp.txt");
+        return 0;
+    }
+
+    if (fgets(line, sizeof(line), f) != NULL) {
+        found = 1;
+    }
+
+    fclose(f);
+    remove("temp.txt");
+    return found;
+}
 
 int main() {
-    printf("Hello world");
+    int a = HasFiles();
+    printf("if theres something in the directory it will show 1");
+    printf("\n%d", a);
 }
